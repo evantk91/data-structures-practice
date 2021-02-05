ff1041e92bca994ab08e6ec378238eb72268b0be
etgreer91 <etgreer91@gmail.com>
2021-02-05 14:13:45 -0700

    updates string permutations problem solution

--- problems/recursion/stringPermutations.js
+++ problems/recursion/stringPermutations.js
@@ -4,17 +4,22 @@ function permute(input) {
 
    let stringPermutations = [];
 
+   //base case - if input is empty, push empty string into permutation list
    if(input.length === 0) {
       stringPermutations.push('');
    } else {
+      //remove first character of the string to be permuted for each substring 
       let first = input[0];
       let remainingStr = input.slice(1, input.length);
       let newStr;
 
+      //call recursively on remaining string
       subStringCombinations = permute(remainingStr);
 
+      //iterate through substring return from recursive call
       for(let i = 0; i < subStringCombinations.length; i++) {
          let string = subStringCombinations[i];
+         //insert first character into all possible positions for each substring
          for(let j = 0; j <= string.length; j++) {
             if(j === 0) {
                newStr = first + string;
@@ -23,6 +28,7 @@ function permute(input) {
             } else {
                newStr = string.substring(0, j) + first + string.substring(j, string.length);
             };
+            //push each new string into permutation list
             stringPermutations.push(newStr);
          };
       }; 
