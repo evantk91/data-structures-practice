b33b1f63c7a879d7318d80b410f9a7f111bc2c40
etgreer91 <etgreer91@gmail.com>
2021-02-10 14:50:26 -0700

    updates keypad solution

--- problems/recursion/keypadCombinations.js
+++ problems/recursion/keypadCombinations.js
@@ -6,32 +6,64 @@ function keypad(num) {
 
    //base case - if input is zero, push empty string into combination list
    if(num === 0) {
-      letterCombinations.push("");          
+      keypadCombinations.push("");          
    } else {
       //remove first digit of the number 
       let numStr = num.toString();
       let firstDigit = parseInt(numStr[0]);
       let remainingDigits;
 
+      //set remaining digits to 0 if the input is a single digit
       if(numStr.length === 1) {
          remainingDigits = 0;    
       } else {
          remainingDigits = parseInt(numStr.slice(1, numStr.length));
       };
 
-      //call recursively on remaining string
-      keypadCombinations = keypad(remainingDigits);
+      //call recursively on remaining digits
+      subCombinations = keypad(remainingDigits);
 
-      for(let i = 0; i < keypadCombinations.length; i++) {
-         let keypadCombination = keypadCombinations[i];
-
+      //get possible characters for the digit
+      let numChars = getCharacters(firstDigit);
+      let newComb;
+      //find all combinations between digit characters and sub combinations 
+      //returned from the recursive call
+      for(let i = 0; i < numChars.length; i++) {
+         let char = numChars[i]; 
+         for(let j = 0; j < subCombinations.length; j++) {
+            let subCombination = subCombinations[j];
+            newComb = char + subCombination;
+            //push each new combination into list
+            keypadCombinations.push(newComb);
+         }
       }
-      
-      console.log(firstDigit);
-      console.log(remainingDigits);
    }
 
    return keypadCombinations;
+}
+
+//helper function that returns the possible characters for the digit
+function getCharacters(num) {
+   switch(num) {
+      case 2:
+         return "abc";
+      case 3:
+         return "def";    
+      case 4:
+         return "ghi";  
+      case 5:
+         return "jkl";
+      case 6:
+         return "mno";    
+      case 7:
+         return "pqrs";
+      case 8:
+         return "tuv";    
+      case 9:
+         return "wxyz"; 
+      default:
+         return "";                             
+   }    
 }
 
 module.exports = {
