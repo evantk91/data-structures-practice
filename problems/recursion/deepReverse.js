b794e7922310b4c4cf86e62f12073f9ff8aa3d3f
etgreer91 <etgreer91@gmail.com>
2021-02-10 15:56:04 -0700

    adds deep reverse problem and tests

---
+++ problems/recursion/deepReverse.js
@@ -0,0 +1,9 @@
+function deepReverse(input) {
+   //takes in a list as input and returns a list that is the deep reverse of the input
+   //if any elements are lists themselves, the elements in the list are reversed    
+
+}
+
+module.exports = {
+   deepReverse,    
+}
---
+++ test/deepReverse.test.js
@@ -0,0 +1,32 @@
+const assert = require('assert');
+const { deepReverse } = require('../problems/recursion/deepReverse.js');
+
+describe('deep reverse tests', () => {
+   it('test case #1', () => {
+      const input = [1, 2, 3, 4, 5];
+      const solution = [5, 4, 3, 2, 1];
+      const output = deepReverse(input);    
+      assert.deepEqual(output, solution);
+   });
+
+   xit('test case #2', () => {
+      const input = [1, 2, [3, 4, 5], 4, 5];
+      const solution = [5, 4, [5, 4, 3], 2, 1];
+      const output = deepReverse(input);    
+      assert.deepEqual(output, solution);    
+   });
+
+   xit('test case #3', () => {
+      const input = [1, [2, 3, [4, [5, 6]]]];
+      const solution = [[[[6, 5], 4] 3, 2], 1];
+      const output = deepReverse(input);
+      assert.deepEqual(output, solution);
+   });
+
+   xit('test case #3', () => {
+      const input = [1, [2, 3], 4, [5, 6]];
+      const solution = [[6, 5], 4, [3, 2], 1];
+      const output = deepReverse(input);
+      assert.deepEqual(output, solution);
+   });
+});
